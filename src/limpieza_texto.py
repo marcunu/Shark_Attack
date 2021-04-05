575b749f3235abd12db22bde004718d696f52d3d
“John <johndoe@example.comclear>
2021-04-05 11:07:01 +0200

    Checkpoint

--- src/limpieza_texto.py
+++ src/limpieza_texto.py
@@ -99,32 +99,6 @@ def df_filtered_best(data_frame, columna, n_ind):
     
     return df_final
 
+def rotar_labels(grafico, angulo):
+    grafico.set_xticklabels(grafico.get_xticklabels(), rotation=angulo)
 
-def grafica_investigador(persona, color):
-    grafi = shrk[shrk["Investigator or Source"] == persona].reset_index()
-    sns.countplot(y=grafi.Country, palette=color)
-
-
-def grafico_lista_h(lista, color):
-
-    df_final = shrk[shrk["Activity"] == lista[0]]
-    lista.pop(0)
-    for i in lista:
-
-        i =shrk[shrk["Activity"] == i]
-        df_final = pd.concat([df_final,i])
-
-    df_final.sample(5)
-    return sns.countplot(y=df_final.Activity, palette=color)
-
-
-def guarda_grafico (nombre, nombre_expor):
-    '''
-    This function saves the graphic.
-    Arguments:
-        -nombre: The name of the graphic you want to save
-        -nombre_expor: choosen name to export.
-
-    '''
-    
-    nombre.figure.savefig(nombre_expor)
--- src/visualizacion.py
+++ src/visualizacion.py
@@ -8,3 +8,23 @@ def guarda_grafico (nombre, nombre_expor):
     '''
     
     nombre.figure.savefig(nombre_expor)
+
+def grafica_investigador(persona, color):
+    grafi = shrk[shrk["Investigator or Source"] == persona].reset_index()
+    sns.countplot(y=grafi.Country, palette=color)
+
+
+def grafico_lista_h(lista, color):
+
+    df_final = shrk[shrk["Activity"] == lista[0]]
+    lista.pop(0)
+    for i in lista:
+
+        i =shrk[shrk["Activity"] == i]
+        df_final = pd.concat([df_final,i])
+
+    df_final.sample(5)
+    return sns.countplot(y=df_final.Activity, palette=color)
+
+def rotar_labels(grafico, angulo):
+    grafico.set_xticklabels(grafico.get_xticklabels(), rotation=angulo)
